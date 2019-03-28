890db268533f7d4e8cfa97b6d37eb5be3fe63b6e
Peter <961154789@qq.com>
2019-03-28 15:36:20 +0800

    99.恢复二叉搜索树

---
+++ recover-binary-search-tree.py
@@ -0,0 +1,30 @@
+# coding=utf-8 
+# @Time :2019/3/28 15:27
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def recoverTree(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        self.n1 = self.n2 = None
+        self.prev = None
+        self.FindTwoNodes(root)
+        self.n1.val, self.n2.val = self.n2.val, self.n1.val
+
+    def FindTwoNodes(self, root):
+        if root:
+            self.FindTwoNodes(root.left)
+            if self.prev and self.prev.val > root.val:
+                self.n2 = root
+                if not self.n1:
+                    self.n1 = self.prev
+            self.prev = root
+            self.FindTwoNodes(root.right)
